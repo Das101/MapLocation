14d63db49659da15c784993b64a1a2b668626048
Alisha Das <99271768+Das101@users.noreply.github.com>
2022-06-03 19:15:01 -0700

    Initial commit

---
+++ app/src/main/java/com/example/maplocation/MainActivity.java
@@ -0,0 +1,212 @@
+package com.example.maplocation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_CODE = 1;
+
+    // Location Demo with FUSED LOCATION PROVIDER CLIENT
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+
+    private static final int UPDATE_INTERVAL = 5000; // 5 seconds
+    private static final int FASTEST_INTERVAL = 3000; // 3 seconds
+
+    private List<String> permissionsToRequest;
+    private List<String> permissions = new ArrayList<>();
+    private List<String> permissionsRejected = new ArrayList<>();
+
+    private TextView location_tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        location_tv = findViewById(R.id.location_tv);
+
+        // instantiate the fusedLocationProviderClient
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // add permissions
+
+        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        permissionsToRequest = permissionsToRequest(permissions);
+        if (permissionsToRequest.size() > 0) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(permissionsToRequest.toArray(new String[permissionsToRequest.size()]), REQUEST_CODE);
+            }
+        }
+    }
+
+    private List<String> permissionsToRequest(List<String> permissions) {
+        ArrayList<String> results = new ArrayList<>();
+        for (String perm : permissions) {
+            if (!hasPermission(perm))
+                results.add(perm);
+        }
+
+        return results;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d(TAG, "onStop: ");
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+    }
+
+    private boolean hasPermission(String perm) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return checkSelfPermission(perm) == PackageManager.PERMISSION_GRANTED;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        // this is a proper place to check the google play services availability
+
+        int errorCode = GoogleApiAvailability.getInstance()
+                .isGooglePlayServicesAvailable(this);
+
+        if (errorCode != ConnectionResult.SUCCESS) {
+            Dialog errorDialog = GoogleApiAvailability.getInstance()
+                    .getErrorDialog(this, errorCode, errorCode, new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialog) {
+                            Toast.makeText(MainActivity.this, "No Services", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+            errorDialog.show();
+        } else {
+            Log.i(TAG, "onPostResume: ");
+            findLocation();
+        }
+
+
+    }
+
+    private void findLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        } else {
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        // here
+                        location_tv.setText(String.format("Lat: %s, Lng: %s altitude: %s accuracy: %s", location.getLatitude(), location.getLongitude(), location.getAltitude(), location.getAccuracy()));
+                    }
+                }
+            });
+        }
+
+        startUpdateLocation();
+
+
+    }
+
+    private void startUpdateLocation() {
+        Log.d(TAG, "startUpdateLocation: ");
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                if (locationResult != null) {
+                    Location location = locationResult.getLastLocation();
+                    // here
+                    location_tv.setText(String.format("Lat: %s, Lng: %s altitude: %s  accuracy:%s", location.getLatitude(), location.getLongitude(),location.getAltitude(), location.getAccuracy()));
+                }
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE) {
+            for (String perm: permissions) {
+                if (!hasPermission(perm))
+                    permissionsRejected.add(perm);
+            }
+
+            if (permissionsRejected.size() > 0 ) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setMessage("The location permission is mandatory")
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                            requestPermissions(permissionsRejected.toArray(new String[permissionsRejected.size()]), REQUEST_CODE);
+                                        }
+
+                                    }
+                                }).setNegativeButton("Cancel", null)
+                                .create()
+                                .show();
+                    }
+                }
+            }
+        }
+    }
+
+
+}
+
